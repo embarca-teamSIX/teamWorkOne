cad500e6dbe4245530dd048cca86185d2280160c
Jorge Palma <jorge.palma@fieb.org.br>
2024-12-18 01:35:33 -0300

    Creating branch and files lib to Temperature conversion

---
+++ Temperature.c
@@ -0,0 +1,45 @@
+/*
+* Made by: Jorge Palma
+* Date:    18/12/2024
+* Project Embarcatech - First activite in group
+*
+* File: Temperature.c
+*/
+
+#include "Temperature.h"
+
+// Converts Celsius to Fahrenheit
+double celsius_to_fahrenheit(double celsius)
+{
+    return 0.0;
+}
+
+// Converts Celsius to Kelvin
+double celsius_to_kelvin(double celsius)
+{
+    return 0.0;
+}
+
+// Converts Fahrenheit to Celsius
+double fahrenheit_to_celsius(double fahrenheit)
+{
+    return 0.0;
+}
+
+// Converts Fahrenheit to Kelvin
+double fahrenheit_to_kelvin(double fahrenheit)
+{
+    return 0.0;
+}
+
+// Converts Kelvin to Celsius
+double kelvin_to_celsius(double kelvin)
+{
+    return 0.0;
+}
+
+// Converts Kelvin to Fahrenheit
+double kelvin_to_fahrenheit(double kelvin)
+{
+    return 0.0;
+}
---
+++ Temperature.h
@@ -0,0 +1,30 @@
+/*
+* Made by: Jorge Palma
+* Date:    18/12/2024
+* Project Embarcatech - First activite in group
+*
+* File: Temperature.h
+*/
+
+#ifndef TEMPERATURE_CONVERSION_H
+#define TEMPERATURE_CONVERSION_H
+
+// Converts Celsius to Fahrenheit
+double celsius_to_fahrenheit(double celsius);
+
+// Converts Celsius to Kelvin
+double celsius_to_kelvin(double celsius);
+
+// Converts Fahrenheit to Celsius
+double fahrenheit_to_celsius(double fahrenheit);
+
+// Converts Fahrenheit to Kelvin
+double fahrenheit_to_kelvin(double fahrenheit);
+
+// Converts Kelvin to Celsius
+double kelvin_to_celsius(double kelvin);
+
+// Converts Kelvin to Fahrenheit
+double kelvin_to_fahrenheit(double kelvin);
+
+#endif // TEMPERATURE_CONVERSION_H
