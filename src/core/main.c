837adca172008bbabe408cac8acb91b4cce50176
Eu, de paula <gfpaula.cic@uesc.br>
2024-12-19 05:19:44 -0300

    updates in main

--- src/core/main.c
+++ src/core/main.c
@@ -1,15 +1,21 @@
+/*
+main/
+    |->menu para selecionar
+    |->funções de loop para interação visual
+    |                   |->apos função ser selecionada 
+    |                   |-> iniciar subfunções para interface de iteração
+    |                   |   da opção desejada EX: interface de ler re retornar valor 
+    |                   |   convertido de cms para kms ou Cº para Fº
+    |                   |-> em caso de erro retornar status do erro e não encerrar
+    |->implementar foward e backward entre os sub menus
+    
+*/
+
 #include <stdio.h>
-#include <conversaoescla.c>
+#include <stdlib.h>
 
 
 int main(){
 
-
-
-    printf("conversao de escala mm,m,cm");
-
-    
-
- return 0;
-    
+ return 0;    
 }
