4faa858f6263777950bd2d33b58cfba9d6207c9d
deividrib <202011130033@ifba.edu.br>
2024-12-24 01:30:55 -0300

    Update conversor.h

--- conversor.h
+++ conversor.h
@@ -1,21 +1,10 @@
 #ifndef CONVERSOR_H
 #define CONVERSOR_H
 
-// Funções de conversão
-void metroParaCentimetro(double *metro) {
-    *metro = (*metro) * 100.0;
-}
-
-void metroParaMilimetro(double *metro) {
-    *metro = (*metro) * 1000.0;
-}
-
-void centimetroParaMetro(double *centimetro) {
-    *centimetro = (*centimetro) / 100.0;
-}
-
-void milimetroParaMetro(double *milimetro) {
-    *milimetro = (*milimetro) / 1000.0;
-}
+// Protótipos das funções de conversão
+void metroParaCentimetro(double *metro);
+void metroParaMilimetro(double *metro);
+void centimetroParaMetro(double *centimetro);
+void milimetroParaMetro(double *milimetro);
 
 #endif
