fb0deb3f0e42c778a7ad35119174cee5c909492b
deividrib <202011130033@ifba.edu.br>
2024-12-24 02:44:55 -0300

    Create conversor.h

---
+++ includes/conversor.h
@@ -0,0 +1,10 @@
+#ifndef CONVERSOR_H
+#define CONVERSOR_H
+
+// Protótipos das funções de conversão
+void metroParaCentimetro(double *metro);
+void metroParaMilimetro(double *metro);
+void centimetroParaMetro(double *centimetro);
+void milimetroParaMetro(double *milimetro);
+
+#endif
