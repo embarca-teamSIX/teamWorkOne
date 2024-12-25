041151c4674d616525eb15c8812f9c8ac3fb9571
Eu, de paula <gfpaula.cic@uesc.br>
2024-12-25 01:34:04 -0300

    new things

---
+++ includes/unidade_de_medida.h
@@ -0,0 +1,10 @@
+void converterLitrosParaMililitros(float litros);
+
+void converterMililitrosParaLitros(float mililitros);
+
+void converterLitrosParaMetrosCubicos(float litros) ;
+
+void converterMetrosCubicosParaLitros(float metrosCubicos) ;
+void converterMililitrosParaMetrosCubicos(float mililitros) ;
+
+void converterMetrosCubicosParaMililitros(float metrosCubicos) ;
---
+++ src/modules/unidade_de_medida.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void converterLitrosParaMililitros(float litros) {
+    printf("%.2f Litros = %.2f Mililitros\n", litros, litros * 1000);
+}
+
+void converterMililitrosParaLitros(float mililitros) {
+    printf("%.2f Mililitros = %.2f Litros\n", mililitros, mililitros / 1000);
+}
+
+void converterLitrosParaMetrosCubicos(float litros) {
+    printf("%.2f Litros = %.6f Metros Cubicos\n", litros, litros / 1000);
+}
+
+void converterMetrosCubicosParaLitros(float metrosCubicos) {
+    printf("%.6f Metros Cubicos = %.2f Litros\n", metrosCubicos, metrosCubicos * 1000);
+}
+
+void converterMililitrosParaMetrosCubicos(float mililitros) {
+    printf("%.2f Mililitros = %.6f Metros Cubicos\n", mililitros, mililitros / 1000000);
+}
+
+void converterMetrosCubicosParaMililitros(float metrosCubicos) {
+    printf("%.6f Metros Cubicos = %.2f Mililitros\n", metrosCubicos, metrosCubicos * 1000000);
+}
+
