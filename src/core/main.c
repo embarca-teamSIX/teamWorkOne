93c8aa98ec73c8b72d704cf4e97c3c86484fbd00
Eu, de paula <gfpaula.cic@uesc.br>
2024-12-26 23:02:32 -0300

    Mais Adequações do cMakeFile para compilação em Windows

--- src/core/main.c
+++ src/core/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <termio.h>
-#include <unistd.h>
+
 
 //macros
 #define OPCOES 7
--- src/core/moduloConversaoArmazenamento.c
+++ src/core/moduloConversaoArmazenamento.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <termio.h>
-#include <unistd.h>
+
 
 #include "armazenamento.h" // Cabeçalho com as funções de conversão de armazenamento.
 #include "MODULO_OPERACAO_ARMAZENAMENTO.h"
